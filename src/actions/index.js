b0e113ed81010f14287fb7b6fc09f310d0b67936
Amit Ranan <ranan.amit@gmail.com>
2016-11-23 13:34:11 -0800

    add fetchWeather action creator and sync to SearchBar

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '9f8096150320ee489d53e2c5af546d53';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}, us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
-}
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
 
   onInputChange(event) {
     this.setState({
@@ -15,9 +19,17 @@ export default class SearchBar extends Component {
     })
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({
+      term: ''
+    });
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
@@ -30,3 +42,9 @@ export default class SearchBar extends Component {
       );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
