ecf49cd90bf1aaaee70eff7978d12562d69dfb9e
Amit Ranan <ranan.amit@gmail.com>
2016-11-22 17:12:48 -0800

    add onInputChange to SearchBar input

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    render() {
-        return (
-            <form className="input-group">
-                <input />
-                <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary">Submit</button>
-                </span>
-            </form>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+}
+
+  onInputChange(event) {
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-secondary">Submit</button>
+          </span>
+      </form>
+      );
+  }
 }
