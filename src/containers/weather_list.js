ad7b99f0f494f1c4137193fa10c4c7dd7fce3d55
Amit Ranan <ranan.amit@gmail.com>
2016-11-27 16:06:58 -0800

    add google_map component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,14 +6,16 @@ class WeatherList extends Component {
 
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5) - 459.67);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units="°F" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
       </tr>
     );
   }
@@ -24,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (°F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
